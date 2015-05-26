1581ddbad8dd508df76c42febb3a70c8fb7c8c9d
stoneyong <stone.zou@mingdao.com>
2015-05-26 20:23:20 +0800

    静态页面

---
+++ Web/js/loverScript.js
@@ -0,0 +1,30 @@
+﻿
+
+$(function () {
+    
+    var Lover = {
+        options:{
+
+        },
+
+        init:function(){
+            this.getInitData();
+        },
+
+        getInitData: function () {
+            $.ajax({
+                url: "",
+                data:{action:"getUserData"},
+                dataType:'JSON',
+                success:function(){
+
+                },
+                error: function () {
+                }
+
+            })
+
+        },
+};
+    return Lover;
+});
